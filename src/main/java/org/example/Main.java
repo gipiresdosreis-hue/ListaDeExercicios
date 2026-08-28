3a2c975fe29924a084438364f2da795fb950c132
gipiresdosreis-hue <gipiresdosreis@gmail.com>
2026-08-28 15:47:04 -0300

    feat: Exercício 04 - nível 04

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        double real;
-        System.out.println("Informe o valor em real: ");
-        real  = entrada.nextDouble();
-        System.out.println("Informe a cotacao atual do dolar : ");
-        double dolar;
-        dolar = entrada.nextDouble();
-        double valorFinal = real / dolar;
-        System.out.println("O valor " + real + " em dolares e: " + valorFinal);
+        double raio;
+        System.out.println("Informe o raio do circulo : ");
+        raio  = entrada.nextDouble();
+        double area;
+        area = 3.14 * raio * raio ;
+        System.out.println("A area do circulo e :" + area);
     }
 }
 

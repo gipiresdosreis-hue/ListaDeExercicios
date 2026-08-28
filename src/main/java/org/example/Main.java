f7fee54f275901cdff5f0f8b4f0b9063f49315fd
gipiresdosreis-hue <gipiresdosreis@gmail.com>
2026-08-28 16:24:50 -0300

    feat: Exercício 03 - nível 05

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,17 +6,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Informe o valor em metros: ");
-        double metros;
-        metros = entrada.nextDouble();
-        double cm;
-        cm = metros * 100;
-        System.out.println("O valor em cm e : " + cm );
-
-
-
-    }
+        System.out.println("Informe o primeiro valor inteiro: ");
+        int valor1;
+        valor1 = entrada.nextInt();
+        System.out.println("Informe o segundo valor: ");
+        int valor2;
+        valor2 = entrada.nextInt();
+        int divisao;
+        divisao = valor1 / valor2;;
+        int qouciente;
+        qouciente = valor1 % valor2;
+        System.out.println("Resultado na divisao: " + divisao);
+        System.out.println("Resultado do quocinte: " + qouciente);
+      }
 }
-
-
-

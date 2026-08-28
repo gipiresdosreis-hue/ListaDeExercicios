d20955e4e041c45ff4128fb39806998a9582d632
gipiresdosreis-hue <gipiresdosreis@gmail.com>
2026-08-28 16:13:50 -0300

    feat: Exercício 02 - nível 05

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,15 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Informe o peso: ");
-        double peso;
-        peso = entrada.nextDouble();
-        System.out.println("Informe a altura: ");
-        double altura;
-        altura = entrada.nextDouble();
-        double imc;
-        imc = peso / altura * altura;
-        System.out.println("O imc e : " + imc );
+        System.out.println("Informe o valor em metros: ");
+        double metros;
+        metros = entrada.nextDouble();
+        double cm;
+        cm = metros * 100;
+        System.out.println("O valor em cm e : " + cm );
 
 
 

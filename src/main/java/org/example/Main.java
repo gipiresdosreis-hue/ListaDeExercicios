14e12e84cad4060c25aa0fa07be5bdfbb61fa4b6
gipiresdosreis-hue <gipiresdosreis@gmail.com>
2026-08-28 14:48:42 -0300

    feat: Add Algoritmo de multiplicacao de dois numeros

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,10 +9,10 @@ public class Main {
         System.out.println("Informe o valor da hora trabalhada : ");
         valorDaHora = entrada.nextDouble();
         System.out.println("Informe o numero de horas trabalhadas no mes  : ");
-        int horaTrabalhada;
-        horaTrabalhada= entrada.nextDouble();
-        double media= distancia / combustivel;
-        System.out.println("O consumo medio de combustivel e : " + media);
+        double horaTrabalhada;
+        horaTrabalhada = entrada.nextDouble();
+        double salario= valorDaHora * horaTrabalhada;
+        System.out.println("O salario bruto e " + salario );
 
 
     }

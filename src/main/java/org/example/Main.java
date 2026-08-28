dc803c744de352e8d5aa757cefea4d17d448b2e1
GIOVANNA PIRES DOS REIS <giovanna.reis11@senaisp.edu.br>
2026-08-28 14:30:39 -0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double valorDaHora;
+        System.out.println("Informe o valor da hora trabalhada : ");
+        valorDaHora = entrada.nextDouble();
+        System.out.println("Informe o numero de horas trabalhadas no mes  : ");
+        int horaTrabalhada;
+        horaTrabalhada= entrada.nextDouble();
+        double media= distancia / combustivel;
+        System.out.println("O consumo medio de combustivel e : " + media);
+
+
+    }
+}

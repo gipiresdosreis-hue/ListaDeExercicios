08ac441ca715ddc492e725ee75c48010ee3a0427
gipiresdosreis-hue <gipiresdosreis@gmail.com>
2026-08-28 16:09:00 -0300

    feat: Exercício 01 - nível 05

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner (System.in);
-        System.out.println( "Qual o valor de A ? ");
-        int A;
-        A = entrada.nextInt();
-        System.out.println( "Qual o valor de B ? ");
-        int B;
-        B = entrada.nextInt();
-        System.out.println("Primeiro valor: A = " + A + " e B = " + B);
+        Scanner entrada = new Scanner(System.in);
 
-        int temporaria = A;
-        A = B;
-        B = temporaria;
-        System.out.println( "Segundo valor: A =  " + A  + " e " + B );
+        System.out.println("Informe o peso: ");
+        double peso;
+        peso = entrada.nextDouble();
+        System.out.println("Informe a altura: ");
+        double altura;
+        altura = entrada.nextDouble();
+        double imc;
+        imc = peso / altura * altura;
+        System.out.println("O imc e : " + imc );
+
 
 
     }

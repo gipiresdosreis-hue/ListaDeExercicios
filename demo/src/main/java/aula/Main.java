1f95a2d685fc8b8611c7caf648d8c642b9d20ec7
GIOVANNA PIRES DOS REIS <giovanna.reis11@senaisp.edu.br>
2026-09-01 14:33:12 -0300

    feat: adiciona exercicio 05 nivel 05

--- demo/src/main/java/aula/Main.java
+++ demo/src/main/java/aula/Main.java
@@ -1,26 +1,19 @@
 package aula;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner (System.in);
 
-        System.out.println("Informe a quantidade de segundos: ");
-        double segundos = entrada.nextDouble();
+        int valorInt = 12;
+        double valorDouble = valorInt;
 
-        double minutos = segundos / 60;
-        double horas = segundos / 3600;
-                System.out.println("Segundos: " + segundos +
-                ", minutos: " + minutos +
-                ", horas: " + horas);
-
-        entrada.close();
+        System.out.println("Valor do Dogras: " + valorDouble);
     }
+}
+
 
 
 
         
 
         
-    }
+    

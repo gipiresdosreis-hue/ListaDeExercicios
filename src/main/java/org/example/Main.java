9358e203e64b6da345b696047deb47b6c2abbdba
50033949859 <gipiresdosreis@gmail.com>
2026-09-02 15:31:54 -0300

    feat: Exercicio 01- nivel 01

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,11 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+        int valorInt = 12;
+        double valorDouble = valorInt;
 
-        Scanner entrada = new Scanner(System.in);
-
-        System.out.println("Informe a quantidade de segundos: ");
-
-        double segundos = entrada.nextDouble();
-
-        double minutos = segundos / 60;
-        double horas = segundos / 3600;
-
-        System.out.println("Segundos: " + segundos +
-                ", minutos: " + minutos +
-                ", horas: " + horas);
-
-        entrada.close();
+        System.out.println("Valor do Dogras " + valorDouble);
     }
 }
 

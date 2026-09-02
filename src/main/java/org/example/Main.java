b6ed26059a90be18cc0dc8dd6c8296fee14f7b89
50033949859 <gipiresdosreis@gmail.com>
2026-09-02 15:32:54 -0300

    feat: Exercicio 02- nivel 01

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int valorInt = 12;
-        double valorDouble = valorInt;
+        double valorDouble = 25.7;
+        int valorInt = (int) valorDouble;
 
-        System.out.println("Valor do Dogras " + valorDouble);
+        System.out.println("Valor do Dogras " + valorInt);
     }
 }
 

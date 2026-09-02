25e65a9ef1b617fb56d246f8d1359fe8fdce6707
50033949859 <gipiresdosreis@gmail.com>
2026-09-02 16:07:24 -0300

    feat: Exercicio 02- nivel 03

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,11 @@
-package org.example;
-public class Main {
+public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    double celcius;
+    System.out.println("Informe a temperatura em graus celcius: ");
+    celcius = entrada.nextDouble();
+    double farenheit = (celcius * 9/5) + 32 ;
+    System.out.println(" A temperatura em farenheit:  " + farenheit );
 
-    public static void main(String[] args) {
-
-        double v1, v2, v3;
-
-        v1 = 75;
-        v2 = 10.7;
-        v3 = 100;
-
-        double media = (v1 + v2 + v3) / 3;
-
-        System.out.println("Media dos valores: " + media);
-    }
-}
 
 
 

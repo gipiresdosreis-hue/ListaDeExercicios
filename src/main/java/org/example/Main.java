c695af5919da6235510efe41ccc21a999df4d914
50033949859 <gipiresdosreis@gmail.com>
2026-09-02 15:28:55 -0300

    feat: Exercicio 05- nivel 05

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,19 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Informe o valor: ");
-        double valor;
-        valor = entrada.nextDouble();
-        System.out.println("Informe a taxa de juros mensal: ");
-        double taxa;
-        taxa = entrada.nextDouble();
-        System.out.print("Informe o tempo em meses: ");
-        double meses;
-        meses = entrada.nextDouble();
-        double juros;
-        juros = valor * taxa * meses;
-        System.out.println("O valor dos juros simples e " + juros );
+        System.out.println("Informe a quantidade de segundos: ");
+
+        double segundos = entrada.nextDouble();
+
+        double minutos = segundos / 60;
+        double horas = segundos / 3600;
+
+        System.out.println("Segundos: " + segundos +
+                ", minutos: " + minutos +
+                ", horas: " + horas);
+
+        entrada.close();
     }
 }
+

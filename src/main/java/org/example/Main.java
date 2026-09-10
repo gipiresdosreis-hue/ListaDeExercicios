332138277a648235f6d5f19a9d87ca9d35cf6739
GIOVANNA PIRES DOS REIS <giovanna.reis11@senaisp.edu.br>
2026-09-10 15:38:45 -0300

    Exercicio de and no java (12)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,24 @@
-package org.example;
+package com.example;
 
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Informe o valor investido pelo cliente:");
 
-        double valor = entrada.nextDouble();
+        System.out.println("Informe a renda mensal: ");
+        double renda = entrada.nextDouble(); 
 
-
-        if (valor <= 10000) {
-
-            System.out.println("Bronze");
-
-        } else if (valor <= 50000){
-            System.out.println("Prata");
+        System.out.println("Informe o score de crédito");
+        double credito = entrada.nextDouble(); 
+        
+        if (renda >= 8000 && credito >= 7000) {
+            System.out.println("Aprovado");
+        } else { 
+            System.out.println("Reprovado");
         }
-        else if (valor <=100000){
-            System.out.println("Ouro");
-        }
-        else {
-            System.out.println("Platinium");
-        }
+ 
 
-        entrada.close();
     }
 }
 
